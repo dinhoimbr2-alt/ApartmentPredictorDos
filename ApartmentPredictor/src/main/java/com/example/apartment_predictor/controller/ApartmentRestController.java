75bc0d1b6fc0319513584f3afbfeb70bb713d585
albertprofe <albertprofe@gmail.com>
2026-01-07 13:22:02 +0100

    feat: apartment api rest findAll, findById

--- ApartmentPredictor/src/main/java/com/example/apartment_predictor/controller/ApartmentRestController.java
+++ ApartmentPredictor/src/main/java/com/example/apartment_predictor/controller/ApartmentRestController.java
@@ -4,16 +4,13 @@ package com.example.apartment_predictor.controller;
 import com.example.apartment_predictor.model.Apartment;
 import com.example.apartment_predictor.service.ApartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/apartment/")
+@RequestMapping("api/apartment")
 public class ApartmentRestController {
 
     @Autowired
@@ -28,7 +25,7 @@ public class ApartmentRestController {
     }
 
 
-    @GetMapping("/getById/{id}")
+    @GetMapping("/getById")
     public Apartment getApartmentById(@RequestParam String id){
 
 
